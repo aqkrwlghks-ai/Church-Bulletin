064a107ade5c22635eabedd31ec118204023de9c
aqkrwlghks <aqkrwlghks@gmail.com>
2026-06-21 02:19:51 +0900

    feat: 구글 스프레드시트 및 Apps Script 연동 완료 (서버리스 전환)

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  // 구글 Apps Script의 웹앱 배포 주소(https://script.google.com/macros/s/.../exec)를 아래 큰따옴표 안에 넣어주세요.
+  // 최초 1회 여기에 주소를 설정한 뒤 깃허브에 푸시하면, 깃허브 페이지를 통해 어디서나 실시간 저장 기능이 가동됩니다.
+  GOOGLE_SCRIPT_API_URL: "https://script.google.com/macros/s/AKfycbz_SAMPLE_URL/exec"
+};
